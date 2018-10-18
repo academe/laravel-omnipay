f87bcf9b8dd71a2121ce6648554b2c8d2d179813
bruhanda <bruhanda@gmail.com>
2018-10-18 19:07:41 +0300

    Update LaravelOmnipayServiceProvider.php

--- src/LaravelOmnipayServiceProvider.php
+++ src/LaravelOmnipayServiceProvider.php
@@ -5,7 +5,7 @@ class LaravelOmnipayServiceProvider extends BaseServiceProvider
     public function boot()
     {
         // Publish config
-        $configPath = __DIR__ . '/../../config/config.php';
+        $configPath = __DIR__ . '/config/config.php';
         $this->publishes([$configPath => config_path('laravel-omnipay.php')], 'config');
     }
 }
